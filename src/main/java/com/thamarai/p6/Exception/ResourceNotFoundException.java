f3fd4b2aaa2a1cfcbcc5290b3a9fb78924839b42
MangooLife <writeandblack@msn.com>
2019-07-28 22:21:49 +0200

    handle error pages

---
+++ src/main/java/com/thamarai/p6/Exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.thamarai.p6.Exception;
+
+public class ResourceNotFoundException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object resourId, Object type) {
+        super(resourId != null ? "This "+ type +" id's missing :"+ resourId : null);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/errorPage.jsp
@@ -0,0 +1,6 @@
+<%@ include file="header.jsp"%>
+	<h1 class="text-center">${errorMsg}</h1>
+	<img src="<c:url value="/resources/image/jeremy-bishop.jpg"/>" 
+		class="rounded mx-auto d-block" height="500" width="350"
+		alt="Error page image">
+<%@ include file="footer.jsp"%>
